5e79d6c8eb15facf8b4aa0316c88659b057095a0
Heo Nayeong <gjsk132@naver.com>
2024-07-08 22:38:28 +0900

    create project

---
+++ app/src/main/java/com/example/alone/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.alone
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.alone.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    val binding : ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
